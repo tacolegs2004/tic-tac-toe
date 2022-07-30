2a0afebe97abd71cecf1d8404bbe8bbf44d4d090
taco <tylerchall16@gmail.com>
2022-07-29 21:11:46 -0500

    On branch master

--- js/Player.js
+++ js/Player.js
@@ -1,52 +1,51 @@
 class Player {
-  constructor(name, id, color, active = false) {
-    this.name = name;
-    this.id = id;
-    this.color = color;
-    this.active = active;
-    this.tokens = this.createTokens(21);
-  }
+	constructor(name, id, color, active = false) {
+		this.name = name;
+		this.id = id;
+		this.color = color;
+		this.active = active;
+		this.tokens = this.createTokens(21);
+	}
 
-  /**
-  * Creates token objects for player
-  * @param   {integer}   num - Number of token objects to be created
-  * @return  {array}     tokens - an array of new token objects
-  */
-  createTokens(num) {
-    const tokens = [];
+	/**
+	 * Creates token objects for player
+	 * @param   {integer}   num - Number of token objects to be created
+	 * @return  {array}     tokens - an array of new token objects
+	 */
+	createTokens(num) {
+		const tokens = [];
 
-    for (let i = 0; i < num; i++) {
-      let token = new Token(i, this)
-      tokens.push(token)
-    }
+		for (let i = 0; i < num; i++) {
+			let token = new Token(i, this);
+			tokens.push(token);
+		}
 
-    return tokens;
-  }
+		return tokens;
+	}
 
-  /**
-   * Gets all tokens that hasn't been dropped.
-   * @return {array}  Array of unused tokens.
-   */
+	/**
+	 * Gets all tokens that hasn't been dropped.
+	 * @return {array}  Array of unused tokens.
+	 */
 
-  get unusedTokens() {
-    return this.tokens.filter(token => !token.dropped);
-  }
+	get unusedTokens() {
+		return this.tokens.filter((token) => !token.dropped);
+	}
 
-  /**
-   * Gets the active token by return the first token in the array of unused tokens.
-   * @return {Object} First token object in the array of unused tokens.
-   */
+	/**
+	 * Gets the active token by return the first token in the array of unused tokens.
+	 * @return {Object} First token object in the array of unused tokens.
+	 */
 
-  get activeToken() {
-    return this.unusedTokens[0]
-    
-  }
+	get activeToken() {
+		return this.unusedTokens[0];
+	}
 
-  /**
-   * Check if a player has any undropped tokens left
-   * @return {boolean}
-   */
-  checkTokens() {
-    return this.unusedTokens.length == 0 ? false : true
-  }
+	/**
+	 * Check if a player has any undropped tokens left
+	 * @return {boolean}
+	 */
+	checkTokens() {
+		return this.unusedTokens.length == 0 ? false : true;
+	}
 }
--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ const game = new Game();
 /**
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
-document.getElementById('begin-game').addEventListener('click', function(){
-  game.startGame();
-  this.style.display = 'none';
-  document.getElementById('play-area').style.opacity = '1';
+document.getElementById("begin-game").addEventListener("click", function () {
+	game.startGame();
+	this.style.display = "none";
+	document.getElementById("play-area").style.opacity = "1";
 });
 
 /**
  * Listens for keyboard presses
  */
-document.addEventListener("keydown", function(e) {
-  game.handleKeydown(e);
-})
+document.addEventListener("keydown", function (e) {
+	game.handleKeydown(e);
+});
